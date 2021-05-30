b813167690afff9e38367e6bde27d9ffa222712f
imsankalp <imsankalpsingh@gmail.com>
2021-05-31 00:47:21 +0530

    ecommerce-web-app v2

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const path = require('path');
 // const seedDB = require('./seed');
 const productRoutes = require('./routes/product');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+
 
 mongoose.connect('mongodb://localhost:27017/shopApp',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify:false
+        useFindAndModify:false,
+        useCreateIndex:true,
     })
     .then(() =>{
         console.log("DB connected");
@@ -24,6 +28,21 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
+const sessionConfig = {
+    secret:"weneedsomebettersecret",
+    resave: false,
+    saveUninitialized: true
+}
+app.use(session(sessionConfig));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
 
 // seedDB();
 
@@ -40,3 +59,7 @@ app.listen(3000, () => {
 
     console.log("Server running at port no 3000");
 })
+
+
+
+
--- routes/product.js
+++ routes/product.js
@@ -4,12 +4,19 @@ const Product = require('../models/product')
 const methodOverride = require('method-override');
 const Review= require('../models/review');
 
-//Show Products
+//Show all Products
 router.get('/products', async (req, res) => {
 
-    const products= await Product.find({});
-    res.render('products/index', {products});
-})
+    try{
+        const products= await Product.find({});
+        res.render('products/index', {products});
+    }
+    catch (e){
+        console.log("Something went wrong");
+        req.flash('error', 'Cannot find products');
+        res.render('error');
+    }
+});
 
 //Create a new product
 router.get('/products/new',  (req, res) => {
@@ -18,50 +25,100 @@ router.get('/products/new',  (req, res) => {
 })
 router.post('/products', async(req, res) => {
 
-    await Product.create(req.body.product);
-    res.redirect('/products');
-})
+    try{
+        await Product.create(req.body.product);
+        req.flash('success','Product created successfully!');
+        res.redirect('/products');
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot create new product! Something went wrong');
+        res.render('error');
+    }
+});
 
 //Show a particular product
 router.get('/products/:id', async (req, res) => {
 
-    const product = await Product.findById(req.params.id).populate('reviews');;
-    res.render('products/show', {product});
+    try{
+        const product = await Product.findById(req.params.id).populate('reviews');;
+        res.render('products/show', {product});    
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot find this Product');
+        res.redirect('/error');
+    }
 })
 
 //Get edit form
 router.get('/products/:id/edit', async(req, res) => {
 
-    const product = await Product.findById(req.params.id);
-    res.render('products/edit', {product});
+    try{
+        const product = await Product.findById(req.params.id);
+        res.render('products/edit', {product});
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot Edit this Product');
+        res.redirect('/error');
+    }
 })
+
+
 //Update the product
 router.patch('/products/:id', async(req, res) => {
 
-    await Product.findByIdAndUpdate(req.params.id, req.body.product);
-    res.redirect('/products');
+    try{
+        await Product.findByIdAndUpdate(req.params.id, req.body.product);
+        req.flash('success', 'Updated Successfully!');
+        res.redirect(`/products/${req.params.id}`) 
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot update this Product');
+        res.redirect('/error');
+    }
 })
 
 //Delete a product
 router.delete('/products/:id', async(req, res) => {
 
-    await Product.findByIdAndDelete(req.params.id);
-    res.redirect('/products');
+    try{
+        await Product.findByIdAndDelete(req.params.id);
+        res.redirect('/products');
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot delete this Product');
+        res.redirect('/error');
+    }
 })
 
 //Creating a new comment
 router.post('/products/:id/review', async (req, res) => {
 
-    const product = await (await Product.findById(req.params.id));
-    const review = new Review(req.body);
+    try{
+        const product = await (await Product.findById(req.params.id));
+        const review = new Review(req.body);
 
-    product.reviews.push(review);
+        product.reviews.push(review);
 
-    await review.save();
-    await product.save();
+        await review.save();
+        await product.save();
 
-    res.redirect(`/products/${req.params.id}`);
+        res.redirect(`/products/${req.params.id}`);
+    }
+    catch(e) {
+        console.log(e.message);
+        req.flash('error', 'Cannot add review to this Product');
+        res.redirect('/error');
+    }
 
+})
+
+router.get('/error', (req, res) => {
+    res.status(404).render('error');
 })
 
 module.exports = router;
